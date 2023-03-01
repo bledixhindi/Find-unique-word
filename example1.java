bef6f5e91664a74b9a307db70895563d008b77d6
Bledjon Xhindi <54402850+bledixhindi@users.noreply.github.com>
2023-03-01 09:56:42 +0100

    Create example1.java

---
+++ example1.java
@@ -0,0 +1,36 @@
+package data;
+
+import java.util.*;
+
+public class example1 {
+	
+	 public static List<String> findUnique(List<String> list){
+		 
+		 List<String> unique = new LinkedList<>();
+		 for (String el : list) {
+			if(!unique.contains(el)) {
+				unique.add(el);
+			}
+			
+		}
+		 
+		 return unique;
+		 
+		 
+	 }
+
+	 public static void main(String[] args) {
+		
+		 String[] words = {"hi" , "hello" , "umt" , "hi"};
+		 List<String> myWords = Arrays.asList(words);
+
+		 List<String> uniqueList = findUnique(myWords);
+		 System.out.println(uniqueList);
+	}
+	 
+	
+
+	
+	
+
+}
